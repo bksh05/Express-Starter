bff4c7eaf2057109c845c88a351355db8e4233a6
Bikash Singh <bksh05@gmail.com>
2020-11-01 20:38:52 +0530

    Adding cors , compression and helmet middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,46 @@
 import {Request , Response} from "express";
 
 const express = require('express');
-
-
-
-
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors')
 const app = express();
 
 
-app.use('/api' , function(request : Request , response : Response ) {
-    response.json({ping : "true"})
+/**
+ * A middleware to compress the body of the response
+ * For more details : https://github.com/expressjs/compression
+ */
+app.use(compression()); 
+
+/**
+ * A middleware to set common headers that can be used to protect against some known attacks
+ * For more details : https://helmetjs.github.io/
+ */
+app.use(helmet());
+
+
+/**
+ * A middleware to specify which application is allowed to recieve the response
+ * Change "http://localhost:4200" to desired url
+ * The package used here provide a lot of configuration and can be modified as per requirement
+ * For more details : https://github.com/expressjs/cors
+ */
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+app.use(cors(corsOptions));
+
+
+
+
+
+
+app.use('/api/ping' , function(request : Request , response : Response ) {
+    response.json({
+        ping : "pinged"
+    })
 })
 
 app.listen(3000 , () => {
